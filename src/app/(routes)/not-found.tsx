e7e9fb1355e5fd0e56e6fabfa5e4301ada223b88
Rose Pace <finally_rose@outlook.com>
2025-01-05 22:57:36 -0500

    Add NotFound page component

---
+++ src/app/(routes)/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// 404 Not Found Page Component
+const NotFound: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+    </div>
+  );
+};
+
+export default NotFound;
